fb8e246dd114002e1142efd3423aa441ccee28ac
aaronh14no <aaron.herbasuucb.edu.bo>
2025-11-17 23:39:31 -0400

    crear usuario con DTO

--- mklite_backend/src/user/user.controller.ts
+++ mklite_backend/src/user/user.controller.ts
@@ -1,34 +1,36 @@
-import {Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
-import { User } from 'src/user/user.entity';
+import { Body, Controller, Post, Get, Param, Delete, Put } from '@nestjs/common';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('/user')
 export class UserController {
-    
-    constructor(private readonly userService: UserService) {}
-    
-   @Post()
-  async createUser(@Body() userData: Partial<User>) {
-    return this.userService.createUser(userData);
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async createUser(@Body() createUserDto: CreateUserDto) {
+    return this.userService.createUser(createUserDto);
   }
 
-    @Get()
-    getAllUsers(){
-        return this.userService.getAllUsers();
-    }
+  @Get()
+  getAllUsers() {
+    return this.userService.getAllUsers();
+  }
 
-    @Get('/:id')
-    getUserById(@Param() params : any){
-        return this.userService.getUserById(params.id);
-    }
+  @Get('/:id')
+  getUserById(@Param('id') id: number) {
+    return this.userService.getUserById(id);
+  }
 
-    @Delete('/:id')
-    deleteUser(@Param() params : any){
-        return this.userService.DeleteUser(params.id);
-    }
+  @Delete('/:id')
+  deleteUser(@Param('id') id: number) {
+    return this.userService.DeleteUser(id);
+  }
 
-    @Put('/:id')
-    updateUser(@Param() params : any, user : User){
-        return this.userService.UpdateUser(params.id, user);
-    }
+  @Put('/:id')
+  updateUser(
+    @Param('id') id: number,
+    @Body() userData: Partial<CreateUserDto>
+  ) {
+    return this.userService.UpdateUser(id, userData);
+  }
 }
